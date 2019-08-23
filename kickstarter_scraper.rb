cdb3a3e82490dafd32d291644b126fea8933b599
Josh Morris <jmo0624@gmail.com>
2019-08-23 00:23:45 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,9 @@ def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
   
   
 end
+
+create_project_hash
